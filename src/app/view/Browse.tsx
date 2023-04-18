9b51c1119dd53802781008cb2b35c92c348fe6cb
Khoon Lin <khoonlin.work@gmail.com>
2023-04-18 14:18:43 +0800

    - Added sorting functionality in browsing view

--- src/app/view/Browse.tsx
+++ src/app/view/Browse.tsx
@@ -47,7 +47,51 @@ function sortRules({
     sortField: Sort;
     sortOrder: SortOrder;
 }): (RevenueRule & { id: number })[] {
-    return rule.slice();
+    return rule.slice().sort((a, b) => {
+        switch (sortField) {
+            case Sort.Id: {
+                return sortOrder ? b.id - a.id : a.id - b.id;
+            }
+            case Sort.Field: {
+                if (sortOrder)
+                    return a.field < b.field ? 1 : a.field === b.field ? 0 : -1;
+                return a.field > b.field ? 1 : a.field === b.field ? 0 : -1;
+            }
+            case Sort.Revenue: {
+                return sortOrder
+                    ? b.revenue - a.revenue
+                    : a.revenue - b.revenue;
+            }
+            case Sort.Operator: {
+                return sortOrder
+                    ? b.operator - a.operator
+                    : a.operator - b.operator;
+            }
+            case Sort.Parameter1: {
+                const right = a.parameter[0] || "";
+                const left = b.parameter[0] || "";
+                if (sortOrder)
+                    return right < left ? 1 : right === left ? 0 : -1;
+                return right > left ? 1 : right === left ? 0 : -1;
+            }
+            case Sort.Parameter2: {
+                const right = a.parameter[1] || "";
+                const left = b.parameter[1] || "";
+                if (sortOrder)
+                    return right < left ? 1 : right === left ? 0 : -1;
+                return right > left ? 1 : right === left ? 0 : -1;
+            }
+            case Sort.Parameter3: {
+                const right = a.parameter[2] || "";
+                const left = b.parameter[2] || "";
+                if (sortOrder)
+                    return right < left ? 1 : right === left ? 0 : -1;
+                return right > left ? 1 : right === left ? 0 : -1;
+            }
+            default:
+                return 0;
+        }
+    });
 }
 
 function SortImageButton({
@@ -70,6 +114,28 @@ function SortImageButton({
     );
 }
 
+const generateOnSort = function (
+    field: Sort,
+    current: Record<Sort, SortOrder | undefined>,
+    dispatch: React.Dispatch<
+        React.SetStateAction<{
+            sortField: Sort;
+            sortOrder: SortOrder;
+        }>
+    >
+) {
+    return function () {
+        const currentOrder = current[field];
+        const newOrder =
+            currentOrder === undefined ? SortOrder.Ascending : 1 - currentOrder;
+        current[field] = newOrder;
+        dispatch({
+            sortField: field,
+            sortOrder: newOrder,
+        });
+    };
+};
+
 function RuleListView({
     rule,
     onRemoveRule,
@@ -83,12 +149,16 @@ function RuleListView({
         sortField: Sort.Id,
         sortOrder: SortOrder.Ascending,
     });
+    const currentSort = React.useRef({
+        [Sort.Id]: SortOrder.Ascending,
+    } as Record<Sort, SortOrder | undefined>);
     const sortedRules = sortRules({
         rule: rule.map((ruleItem, i) => {
             return { ...ruleItem, id: i };
         }),
         ...sort,
     });
+
     const rulesLength = sortedRules.length;
 
     return (
@@ -96,31 +166,80 @@ function RuleListView({
             <div className={"rule-list-title"} key={`rule-title`}>
                 <div className={"rule-id"}>
                     <div>{"Rule"}</div>
-                    <SortImageButton disabled={disabled} />
+                    <SortImageButton
+                        disabled={disabled}
+                        onClick={generateOnSort(
+                            Sort.Id,
+                            currentSort.current,
+                            setSort
+                        )}
+                    />
                 </div>
                 <div className={"rule-field"}>
                     <div>{"Field"}</div>
-                    <SortImageButton disabled={disabled} />
+                    <SortImageButton
+                        disabled={disabled}
+                        onClick={generateOnSort(
+                            Sort.Field,
+                            currentSort.current,
+                            setSort
+                        )}
+                    />
                 </div>
                 <div className={"rule-operator"}>
                     <div>{"Operator"}</div>
-                    <SortImageButton disabled={disabled} />
+                    <SortImageButton
+                        disabled={disabled}
+                        onClick={generateOnSort(
+                            Sort.Operator,
+                            currentSort.current,
+                            setSort
+                        )}
+                    />
                 </div>
                 <div className={"rule-parameter-1"}>
                     <div>{"Parameter 1"}</div>
-                    <SortImageButton disabled={disabled} />
+                    <SortImageButton
+                        disabled={disabled}
+                        onClick={generateOnSort(
+                            Sort.Parameter1,
+                            currentSort.current,
+                            setSort
+                        )}
+                    />
                 </div>
                 <div className={"rule-parameter-2"}>
                     <div>{"Parameter 2"}</div>
-                    <SortImageButton disabled={disabled} />
+                    <SortImageButton
+                        disabled={disabled}
+                        onClick={generateOnSort(
+                            Sort.Parameter2,
+                            currentSort.current,
+                            setSort
+                        )}
+                    />
                 </div>
                 <div className={"rule-parameter-3"}>
                     <div>{"Parameter 3"}</div>
-                    <SortImageButton disabled={disabled} />
+                    <SortImageButton
+                        disabled={disabled}
+                        onClick={generateOnSort(
+                            Sort.Parameter3,
+                            currentSort.current,
+                            setSort
+                        )}
+                    />
                 </div>
                 <div className={"revenue"}>
                     <div>{"Revenue"}</div>
-                    <SortImageButton disabled={disabled} />
+                    <SortImageButton
+                        disabled={disabled}
+                        onClick={generateOnSort(
+                            Sort.Revenue,
+                            currentSort.current,
+                            setSort
+                        )}
+                    />
                 </div>
                 <div className={"action"}>{"Action"}</div>
             </div>
